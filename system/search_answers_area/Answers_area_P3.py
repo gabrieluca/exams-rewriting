3b14d12abd8bdd1278fabb9b7fa57633663a568a
Ricardo Henrique Avelar Matheus <uniermatheus@fei.edu.br>
2019-12-27 19:13:12 -0300

    adiçãode pdfs e aumento na precisão da área de busca

--- system/search_answers_area/Answers_area_P3.py
+++ system/search_answers_area/Answers_area_P3.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("database_files/exams/NM7510_P3_2019_2/NM7510_P3_pacote_A_2_sem_2019-31.png",1) 
+img = cv2.imread("database_files/exams/NM7510_P3_2019_2/NM7510_P3_pacote_A_2_sem_2019-34.png",1) 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('database_files/track_markers/template3.png',cv2.IMREAD_GRAYSCALE)
@@ -23,8 +23,8 @@ for pt in zip(*loc2[::-1]):
     cv2.rectangle(img, pt, (pt[0] + r, pt[1] + f), (0, 255, 0), 3)
 
 if pt[0]>1600:
-    img1 = img[int(pt[0]/50)+w:rt[1] + int(f/1.5), pt[0]:rt[0]+w]
-    img2 = img[int(pt[0]/50)+w+1630:rt[1] + int(f/1.5), pt[0]-1650:rt[0]+w-1650]
+    img1 = img[int(pt[0]/50)+int(w*1.8):rt[1] + int(f/1.6), pt[0]+int(w*0.7):rt[0]+int(w*0.5)]
+    img2 = img[int(pt[0]/50)+w+1630:rt[1] + int(f/2.2), pt[0]-1625:rt[0]+w-1685]
     img1 = cv2.resize(img1, (1080, 660))
     img2 = cv2.resize(img2, (1080, 660))
     cv2.imshow("img", img1)
@@ -33,8 +33,8 @@ if pt[0]>1600:
     cv2.waitKey(0)
 else: 
     if rt[0]<1650:
-        img1 = img[int(pt[0]/50)+w+1512:rt[1] + f, pt[0]:rt[0]+int(w/1.5)]
-        img2 = img[int(pt[0]/50)+w:rt[1] + f, pt[0]+1650:rt[0]+w+1645]
+        img1 = img[int(pt[0]/50)+w+1525:rt[1] + int(f*0.5), pt[0]+int(w*0.4):rt[0]+int(w/2.5)]
+        img2 = img[int(pt[0]/50)+int(w*1.8):rt[1] + int(f*0.5), pt[0]+1680:rt[0]+w+1625]
         img1 = cv2.resize(img1, (1080, 660))
         img2 = cv2.resize(img2, (1080, 660))
         cv2.imshow("img", img1)
@@ -42,8 +42,8 @@ else:
         print('{0}, {1}, {2}, {3}'.format(pt[0],pt[1],rt[0],rt[1]))
         cv2.waitKey(0)
     else:
-        img1 = img[int(pt[0]/50)+w+1650:rt[1] + f, pt[0]:rt[0]+int(w/1.5)-1650]
-        img2 = img[int(pt[0]/50)+w:rt[1] + f, pt[0]+1650:rt[0]+w+1645]
+        img1 = img[int(pt[0]/50)+w+1660:rt[1] + int(f*0.45), pt[0]+int(w*0.5):rt[0]+int(w/1.5)-1658]
+        img2 = img[int(pt[0]/50)+int(w*2.2):rt[1] + int(f*0.5), pt[0]+1687:rt[0]+int(w*0.5)]
         img1 = cv2.resize(img1, (1080, 660))
         img2 = cv2.resize(img2, (1080, 660))
         cv2.imshow("img", img1)
