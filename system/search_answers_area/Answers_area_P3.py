9f6fde7289f8352229ac97e0c564bb9a005840cd
Ricardo Henrique Avelar Matheus <uniermatheus@fei.edu.br>
2019-12-27 19:53:09 -0300

    correções de parametros

--- system/search_answers_area/Answers_area_P3.py
+++ system/search_answers_area/Answers_area_P3.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("database_files/exams/NM7510_P3_2019_2/NM7510_P3_pacote_A_2_sem_2019-43.png",1) 
+img = cv2.imread("database_files/exams/NM7510_P3_2019_2/NM7510_P3_pacote_A_2_sem_2019-31.png",1) 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('database_files/track_markers/template3.png',cv2.IMREAD_GRAYSCALE)
@@ -24,7 +24,7 @@ for pt in zip(*loc2[::-1]):
 
 if pt[0]>1600:
     img1 = img[int(pt[0]/50)+w:rt[1] + int(f/1.5), pt[0]:rt[0]+w]
-    img2 = img[int(pt[0]/50)+w+1610:rt[1] + int(f/1.5), pt[0]-1650:rt[0]+w-1650]
+    img2 = img[int(pt[0]/50)+w+1630:rt[1] + int(f/1.5), pt[0]-1650:rt[0]+w-1650]
     img1 = cv2.resize(img1, (1080, 660))
     img2 = cv2.resize(img2, (1080, 660))
     cv2.imshow("img", img1)
