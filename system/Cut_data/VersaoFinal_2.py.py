b84ba61c06731e4e0063e5c86294bba39a5ff59e
Natalia Souza <natalias18@outlook.com>
2020-01-10 18:40:54 -0200

    Recorte dos dados nas provas P3

--- system/Cut_data/VersaoFinal_2.py.py
+++ system/Cut_data/VersaoFinal_2.py.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("database_files/exams/NM7510_P3_2019_2/NM7510_P3_pacote_A_2_sem_2019-19.png",1) 
+img = cv2.imread("database_files/exams/NM7510_P3_2019_2/NM7510_P3_pacote_A_2_sem_2019-31.png",1) 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('database_files/track_markers/template3.png',cv2.IMREAD_GRAYSCALE)
@@ -26,24 +26,24 @@ if pt [0]<1600:
     print("{},{},{},{}".format(rt[0],rt[1],pt[0],pt[1]))
     img = img[pt[0]+int(w*1.5):pt[0]+340, pt[0]+w:rt[0]]
 
-    RA=img [15:135, 200:920]
-    cv2.imshow("RAP", RA)
+    RA=img [10:135, 200:920]
+    cv2.imshow("RA", RA)
     cv2.waitKey(0)
 
-    Sequencial=img [15:125, 950:1220]
-    cv2.imshow("SequencialP", Sequencial)
+    Sequencial=img [10:125, 950:1220]
+    cv2.imshow("Sequencial", Sequencial)
     cv2.waitKey(0)
 
     Pacote=img [10:135, 1250:1380]
-    cv2.imshow("PacoteP", Pacote)
+    cv2.imshow("Pacote", Pacote)
     cv2.waitKey(0)
 
-    Nome=img [130:250, 60:640]
-    cv2.imshow("NomeP", Nome)
+    Nome=img [123:250, 60:640]
+    cv2.imshow("Nome", Nome)
     cv2.waitKey(0)
 
-    Assinatura=img [120:245, 825:1420]
-    cv2.imshow("AssinaturaP", Assinatura)
+    Assinatura=img [115:245, 825:1420]
+    cv2.imshow("Assinatura", Assinatura)
     cv2.waitKey(0)
 
 else:
@@ -53,23 +53,23 @@ else:
     img = img[pt[0]+int(w*1.5)-1650:pt[0]+340-1630, pt[0]+w-1650:rt[0]-1630]
 
     RA=img [45:150, 200:950]
-    cv2.imshow("RAI", RA)
+    cv2.imshow("RA", RA)
     cv2.waitKey(0)
 
     Sequencial=img [45:150, 980:1250]
-    cv2.imshow("SequencialI", Sequencial)
+    cv2.imshow("Sequencial", Sequencial)
     cv2.waitKey(0)
 
     Pacote=img [45:150, 1280:1380]
-    cv2.imshow("PacoteI", Pacote)
+    cv2.imshow("Pacote", Pacote)
     cv2.waitKey(0)
 
     Nome=img [155:265, 75:670]
-    cv2.imshow("NomeI", Nome)
+    cv2.imshow("Nome", Nome)
     cv2.waitKey(0)
 
     Assinatura=img [155:265, 850:1440]
-    cv2.imshow("AssinaturaI", Assinatura)
+    cv2.imshow("Assinatura", Assinatura)
     cv2.waitKey(0)
 
 cv2.destroyAllWindows()
