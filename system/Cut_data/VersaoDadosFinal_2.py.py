2ee0bfebe452048768fd0b7e94290b4d7e1f28f3
Natalia Souza <natalias18@outlook.com>
2020-01-18 19:38:27 -0200

    Correção do algoritmo

--- system/Cut_data/VersaoDadosFinal_2.py.py
+++ system/Cut_data/VersaoDadosFinal_2.py.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("database_files/exams/NM7510_P3_2019_2/NM7510_P3_pacote_A_2_sem_2019-31.png",1) 
+img = cv2.imread("database_files/exams/NM7510_P3_C_2019_2/NM7510_P3_pacote_C_2_sem_2019-05.png",1) 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-template = cv2.imread('database_files/track_markers/template3.png',cv2.IMREAD_GRAYSCALE)
+template = cv2.imread('database_files/track_markers/template1.png',cv2.IMREAD_GRAYSCALE)
 w, h = template.shape[::-1]
 
 result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
@@ -13,7 +13,7 @@ loc = np.where(result >= 0.659)
 for rt in zip(*loc[::-1]):
     cv2.rectangle(img, rt, (rt[0] + w, rt[1] + h), (0, 255, 0), 3)
 
-template2 = cv2.imread('database_files/track_markers/template4.png',cv2.IMREAD_GRAYSCALE)
+template2 = cv2.imread('database_files/track_markers/template2.png',cv2.IMREAD_GRAYSCALE)
 r, f = template2.shape[::-1]
 
 result2 = cv2.matchTemplate(gray_img, template2, cv2.TM_CCOEFF_NORMED)
