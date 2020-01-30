429f94b69f1e165bbd9332f521d4309d9d878bb7
Ricardo Henrique Avelar Matheus <uniermatheus@fei.edu.br>
2020-01-29 21:51:46 -0300

    Adição de outra possibilidade de algoritmo

--- system/search_answers_area/Contornos/Answers_area_P3.py
+++ system/search_answers_area/Contornos/Answers_area_P3.py
@@ -3,18 +3,18 @@ import os
 import numpy as np
 
 from Normalize_and_crop import normalize_crop
-from P3_par import main_par
+from P3_par_contornos import main_par
 
-path = "database_files/exams/NM7510_P3_A_2019_2/NM7510_P3_pacote_A_2_sem_2019-07.png"
+path = "database_files/exams/NM7510_P3_A_2019_2/NM7510_P3_pacote_A_2_sem_2019-08.png"
 template = cv2.imread('database_files/track_markers/template.png',cv2.IMREAD_GRAYSCALE)
 template2 = cv2.imread('database_files/track_markers/template2.png',cv2.IMREAD_GRAYSCALE)
 img = cv2.imread(path,1)
 # Exam Pack
 t = str(path[57:58])
 # Page number
-x = str(path[70:72])
+n = str(path[70:72])
 # See if its as even number
-y = int(path[70:72]) % 2
+p = int(path[70:72]) % 2
 
 
 def main_impar():
@@ -25,20 +25,32 @@ def main_impar():
     img_res = img_qdri[:, 285:570]
     img_res = cv2.cvtColor(img_res, cv2.COLOR_BGR2GRAY)
 
+    # Create target Directory
+    try:
+        os.mkdir("C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/Contour/{0}_{1}".format(t, n))
+        print("Directory ",t,"_",n," Created ") 
+    except FileExistsError:
+        print("Directory " , t ,"_", n ,  " already exists")
+    path = "C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/Contour/{0}_{1}".format(t, n)
+
+    # Save images in the target Directory
+    cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_1({0}).png'.format(n)), ares1)
+    cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_2({0}).png'.format(n)), ares2)
+
+    #Show images
+    ares1 = ares1[::2,::2]
+    ares2 = ares2[::3,::3]
+    cv2.imshow("Area de resolucao folha 1", ares1)
+    cv2.imshow("area de resolucao folha 2", ares2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     # Normalize and threshold image
     res, im = cv2.threshold(img_res, 166, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-    # Fill everything that is the same colour (black) as top-left corner with white
-    #cv2.floodFill(im, None, (0, 0), 255)
-
-    # Fill everything that is the same colour (white) as top-left corner with black
-    #cv2.floodFill(im, None, (0, 0), 0)
 
     # morphology operation
     kernel = np.ones((3, 3), np.uint8)
     im = cv2.dilate(im, kernel, iterations=1)
-
-    #imgFI = cv2.bitwise_not(im)
 
     # find all your connected components (white blobs in your image)
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(im, connectivity=8)
@@ -61,25 +73,8 @@ def main_impar():
     im2, contours, hierarchy = cv2.findContours(temp, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # List the names for each contour
-    questoes = ['e)','d)','c)','b)','a)','a2)']
+    questoes = ['e','d','c','b','a']
     f=0
-
-    # Create target Directory
-    # try:
-    #     os.mkdir("C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/{0}_{1}".format(t, x))
-    #     print("Directory ",t,"_",x," Created ") 
-    # except FileExistsError:
-    #     print("Directory " , t ,"_", x ,  " already exists")
-
-    # # Save images in the target Directory
-    # path = "C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/{0}_{1}".format(t, x)
-    # cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_1({0}).png'.format(x)), ares1)
-    # cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_2({0}).png'.format(x)), ares2)
-    # cv2.imwrite(os.path.join(path, 'a({0}).png'.format(x)), imgFI[:65,:])
-    # cv2.imwrite(os.path.join(path, 'b({0}).png'.format(x)), imgFI[65:130,:])
-    # cv2.imwrite(os.path.join(path, 'c({0}).png'.format(x)), imgFI[130:195,:])
-    # cv2.imwrite(os.path.join(path, 'd({0}).png'.format(x)), imgFI[195:260,:])
-    # cv2.imwrite(os.path.join(path, 'e({0}).png'.format(x)), imgFI[260:,:])
 
     #for each contour analize if it has the min shape and crop the final image
     for c in contours:
@@ -92,32 +87,12 @@ def main_impar():
             resp = im[ y+2 : y+h-2 , x+2 : x+w-2 ]
             resp = cv2.bitwise_not(resp)
             cv2.imshow(questoes[0+f],resp)
+            cv2.imwrite(os.path.join(path, '{0}({1}).png'.format(questoes[0+f],n)), resp)
             cv2.waitKey(0)
             f+=1
     cv2.destroyAllWindows()
 
-    # Show images
-    #ares1 = ares1[::2,::2]
-    #ares2 = ares2[::3,::3]
-    #cv2.imshow("Area de resolucao folha 1", ares1)
-    #cv2.imshow("area de resolucao folha 2", ares2)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    #cv2.imshow("respostas:", img_res)
-
-    #cv2.imshow("a)", imgFI[:65,:])
-    #cv2.waitKey(0)
-    #cv2.imshow("b)", imgFI[65:130,:])
-    #cv2.waitKey(0)
-    #cv2.imshow("c)", imgFI[130:195,:])
-    #cv2.waitKey(0)
-    #cv2.imshow("d)", imgFI[195:260,:])
-    #cv2.waitKey(0)
-    #cv2.imshow("e)", imgFI[260:,:])
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-if y == 0:
-    main_par(img,template,template2,t,x)
+if p == 0:
+    main_par(img,template,template2,t,n)
 else:
     main_impar()
--- system/search_answers_area/Contornos/P3_par_contornos.py
+++ system/search_answers_area/Contornos/P3_par_contornos.py
@@ -12,8 +12,73 @@ def main_par(img,template,template2,Exam_pack,Page_number):
     img_res = img_qdrp[:, 105:570]
     img_res = cv2.cvtColor(img_res, cv2.COLOR_BGR2GRAY)
 
+    # Create target Directory
+    try:
+        os.mkdir("C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/Contour/{0}_{1}".format(Exam_pack, Page_number))
+        print("Directory " , Exam_pack ,"_", Page_number ,  " Created ") 
+    except FileExistsError:
+        print("Directory " , Exam_pack ,"_", Page_number ,  " already exists")
+    path = "C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/Contour/{0}_{1}".format(Exam_pack, Page_number)
+
+    # Save images in the target Directory
+    cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_1({0}).png'.format(Page_number)), ares1)
+    cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_2({0}).png'.format(Page_number)), ares2)
+
+    # Show images
+    ares1 = ares1[::2,::2]
+    ares2 = ares2[::3,::3]
+    cv2.imshow("Area de resolucao folha 1", ares1)
+    cv2.imshow("area de resolucao folha 2", ares2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     # Normalize and threshold image
     res, im = cv2.threshold(img_res, 166, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    # morphology operation
+    kernel = np.ones((3, 3), np.uint8)
+    im = cv2.dilate(im, kernel, iterations=1)
+
+    img_res2 = im[:, 230:]
+
+    # find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(img_res2, connectivity=8)
+    sizes = stats[1:, -1]
+    nb_components = nb_components - 1
+
+    # minimum size of particles we want to keep (number of pixels)
+    # here, it's a fixed value, but you can set it as you want, eg the mean of the sizes or whatever
+    min_size = 80
+
+    # your answer image
+    temp = np.zeros(output.shape, dtype=np.uint8)
+    # for every component in the image, you keep it only if it's above min_size
+    for j in range(0, nb_components):
+        if sizes[j] >= min_size:
+            temp[output == j + 1] = 255
+    temp = cv2.bitwise_not(temp)
+
+    # Find contours
+    im2, contours, hierarchy = cv2.findContours(temp, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # List the names for each contour
+    questoes = ['e','d','c','b','a']
+    f=0
+
+    #for each contour analize if it has the min shape and crop the final image
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        resp = img_res2[ y : y+h , x : x+w ]
+        a,b = resp.shape
+        if a < 30 or b < 150:
+            d=0
+        elif a>40 and b>200 :
+            resp = img_res2[ y+2 : y+h-2 , x+2 : x+w-2 ]
+            resp = cv2.bitwise_not(resp)
+            cv2.imshow(questoes[0+f],resp)
+            cv2.imwrite(os.path.join(path, '{0}({1}).png'.format(questoes[0+f],Page_number)), resp)
+            cv2.waitKey(0)
+            f+=1
 
     # Fill everything that is the same colour (black) as top-left corner with white
     cv2.floodFill(im, None, (0, 0), 255)
@@ -21,48 +86,9 @@ def main_par(img,template,template2,Exam_pack,Page_number):
     # Fill everything that is the same colour (white) as top-left corner with black
     cv2.floodFill(im, None, (0, 0), 0)
 
-    # morphology operation
-    kernel = np.ones((3, 3), np.uint8)
-    im = cv2.dilate(im, kernel, iterations=1)
-
     imgFI = cv2.bitwise_not(im)
 
-    # Create target Directory
-    try:
-        os.mkdir("C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/{0}_{1}".format(Exam_pack, Page_number))
-        print("Directory " , Exam_pack ,"_", Page_number ,  " Created ") 
-    except FileExistsError:
-        print("Directory " , Exam_pack ,"_", Page_number ,  " already exists")
-
-    # Save images in the target Directory
-    path = "C:/Users/Windows/Documents/GitHub/exams-system/Images/Answers/{0}_{1}".format(Exam_pack, Page_number)
-    cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_1({0}).png'.format(Page_number)), ares1)
-    cv2.imwrite(os.path.join(path, 'Area_resolucao_folha_2({0}).png'.format(Page_number)), ares2)
-    cv2.imwrite(os.path.join(path, 'a1({0}).png'.format(Page_number)), imgFI[:55,50:232])
-    cv2.imwrite(os.path.join(path, 'a2({0}).png'.format(Page_number)), imgFI[:55,232:])
-    cv2.imwrite(os.path.join(path, 'b({0}).png'.format(Page_number)), imgFI[55:110,232:])
-    cv2.imwrite(os.path.join(path, 'c({0}).png'.format(Page_number)), imgFI[110:165,232:])
-    cv2.imwrite(os.path.join(path, 'd({0}).png'.format(Page_number)), imgFI[165:220,232:])
-    cv2.imwrite(os.path.join(path, 'e({0}).png'.format(Page_number)), imgFI[220:,232:])
-
-    ares1 = ares1[::2,::2]
-    ares2 = ares2[::3,::3]
-    cv2.imshow("Area de resolucao folha 1", ares1)
-    cv2.imshow("area de resolucao folha 2", ares2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    cv2.imshow("respostas:", img_res)
-
-    cv2.imshow("a1)", imgFI[:55,50:232])
-    cv2.waitKey(0)
-    cv2.imshow("a2)", imgFI[:55,232:])
-    cv2.waitKey(0)
-    cv2.imshow("b)", imgFI[55:110,232:])
-    cv2.waitKey(0)
-    cv2.imshow("c)", imgFI[110:165,232:])
-    cv2.waitKey(0)
-    cv2.imshow("d)", imgFI[165:220,232:])
-    cv2.waitKey(0)
-    cv2.imshow("e)", imgFI[220:,232:])
+    cv2.imshow("a2({0})".format(Page_number), imgFI[:55,50:232])
+    cv2.imwrite(os.path.join(path, 'a2({0}).png'.format(Page_number)), imgFI[:55,50:232])
     cv2.waitKey(0)
     cv2.destroyAllWindows()
